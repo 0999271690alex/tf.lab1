c233dfeef35bf097b575dfca39edff7530e632c1
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-02-21 16:45:51 +0200

    Update variables.tf

--- lab4/variables.tf
+++ lab4/variables.tf
@@ -1,4 +1,11 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket to which write access will be granted."
   type        = string
+  default     = "cmtr-pf5k68pq-bucket-1771684355"
+}
+
+variable "project_tag" {
+  description = "Project name for tagging resources"
+  type        = string
+  default     = "cmtr-pf5k68pq"
 }
