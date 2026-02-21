b0503383e82304494a9b35fed1d5ac663d667185
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-02-21 22:44:31 +0200

    Update data.tf

--- lab7/data.tf
+++ lab7/data.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "base_infra" {
     region = var.aws_region
   }
 }
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
