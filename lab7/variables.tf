3f12e59e5b50ef8e9174d7ae5542e376f93072f2
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-02-21 22:43:19 +0200

    Update variables.tf

--- lab7/variables.tf
+++ lab7/variables.tf
@@ -1,15 +1,17 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "project_id" {
   description = "Project identifier for tagging"
   type        = string
+  default     = "cmtr-pf5k68pq"
 }
 
 variable "state_bucket" {
-  description = "S3 bucket name for remote Terraform state"
+  description = "S3 bucket name for remote state"
   type        = string
 }
 
