26e5dc8477e33e5739707fd82007a0bd5f74e724
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-02-21 21:12:29 +0200

    Add default values for VPC variables

--- lab6/variables.tf
+++ lab6/variables.tf
@@ -1,11 +1,13 @@
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
+  default     = "cmtr-pf5k68pq-01-vpc"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block of the VPC"
   type        = string
+  default     = "10.10.0.0/16"
 }
 
 variable "public_subnets" {
@@ -15,4 +17,22 @@ variable "public_subnets" {
     cidr_block        = string
     availability_zone = string
   }))
+
+  default = [
+    {
+      name              = "cmtr-pf5k68pq-01-subnet-public-a"
+      cidr_block        = "10.10.1.0/24"
+      availability_zone = "us-east-1a"
+    },
+    {
+      name              = "cmtr-pf5k68pq-01-subnet-public-b"
+      cidr_block        = "10.10.3.0/24"
+      availability_zone = "us-east-1b"
+    },
+    {
+      name              = "cmtr-pf5k68pq-01-subnet-public-c"
+      cidr_block        = "10.10.5.0/24"
+      availability_zone = "us-east-1c"
+    }
+  ]
 }
