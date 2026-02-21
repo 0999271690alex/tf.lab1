4f89a6fe9bbb49a39bc8a795727d2ea3fbb7a057
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-02-21 22:45:03 +0200

    Update compute.tf

--- lab7/compute.tf
+++ lab7/compute.tf
@@ -1,19 +1,13 @@
-provider "aws" {
-  region = var.aws_region
-}
+resource "aws_instance" "public_instance" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
 
-resource "aws_instance" "remote_ec2" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
-  instance_type = "t2.micro"
-
-  subnet_id = data.terraform_remote_state.base_infra.outputs.public_subnet_id
-
-  vpc_security_group_ids = [
-    data.terraform_remote_state.base_infra.outputs.security_group_id
-  ]
+  # Використання даних з remote state
+  subnet_id              = data.terraform_remote_state.base_infra.outputs.public_subnet_id
+  vpc_security_group_ids = [data.terraform_remote_state.base_infra.outputs.security_group_id]
 
   tags = {
-    Name      = "${var.project_id}-remote-ec2"
+    Name      = "${var.project_id}-instance"
     Terraform = "true"
     Project   = var.project_id
   }
