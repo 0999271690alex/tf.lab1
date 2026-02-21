c0a012b37ca7b6a60b01322631aa2c4865ef5c4a
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-02-21 14:55:55 +0200

    Define outputs for VPC and related resources
    Add outputs for VPC, public subnets, internet gateway, and route table.

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = [for subnet in aws_subnet.public : subnet.id]
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public_rt.id
+}
