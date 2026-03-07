4a2e32af3bba2064435cf379618e3ae2db62c5ac
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-07 11:55:20 +0200

    Update data.tf

--- lab9/data.tf
+++ lab9/data.tf
@@ -1,3 +1,34 @@
 provider "aws" {
   region = var.aws_region
 }
+
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnet" "public" {
+  filter {
+    name   = "tag:Name"
+    values = [var.public_subnet_name]
+  }
+}
+
+data "aws_security_group" "instance_sg" {
+  filter {
+    name   = "tag:Name"
+    values = [var.security_group_name]
+  }
+}
+
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
