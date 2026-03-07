8f03fb2e9e06d3a60a1c7b9f82df55703bd2ccf3
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-07 18:17:45 +0200

    Update alb.tf

--- lab13/blue-green/alb.tf
+++ lab13/blue-green/alb.tf
@@ -11,3 +11,38 @@ resource "aws_lb_target_group" "green" {
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.main.id
 }
+
+resource "aws_lb" "main" {
+  name               = "cmtr-pf5k68pq-lb"
+  internal           = false
+  load_balancer_type = "application"
+
+  subnets = [
+    data.aws_subnet.public1.id,
+    data.aws_subnet.public2.id
+  ]
+
+  security_groups = [data.aws_security_group.lb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = var.blue_weight
+      }
+
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = var.green_weight
+      }
+    }
+  }
+}
