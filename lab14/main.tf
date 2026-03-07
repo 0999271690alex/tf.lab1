295c7fc32c1fe7f9d469669acc3e291b3c0c5116
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-07 19:04:39 +0200

    Create main.tf

---
+++ lab14/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "network" {
+  source = "./modules/network"
+}
+
+module "network_security" {
+  source = "./modules/network_security"
+
+  vpc_id           = module.network.vpc_id
+  allowed_ip_range = var.allowed_ip_range
+}
+
+module "application" {
+  source = "./modules/application"
+
+  vpc_id               = module.network.vpc_id
+  subnet_ids           = module.network.subnet_ids
+  ssh_sg               = module.network_security.ssh_sg
+  public_http_sg       = module.network_security.public_http_sg
+  private_http_sg      = module.network_security.private_http_sg
+}
