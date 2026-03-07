21b901ec53f0b30ab076157e34570a4448c6ac94
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-07 18:14:56 +0200

    Create variables.tf

---
+++ lab13/blue-green/variables.tf
@@ -0,0 +1,11 @@
+variable "blue_weight" {
+  description = "Traffic weight for Blue"
+  type        = number
+  default     = 100
+}
+
+variable "green_weight" {
+  description = "Traffic weight for Green"
+  type        = number
+  default     = 0
+}
