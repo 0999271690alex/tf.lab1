2bb4a0776fdacd6d87ad22e74956a23984976dd7
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-07 19:34:27 +0200

    Update variables.tf

--- lab14/modules/application/variables.tf
+++ lab14/modules/application/variables.tf
@@ -1,14 +1,24 @@
+variable "vpc_id" {
+  description = "VPC ID for the application resources"
+  type        = string
+}
+
 variable "subnet_ids" {
-  description = "Subnet IDs"
+  description = "Subnet IDs for the autoscaling group"
   type        = list(string)
 }
 
 variable "ssh_sg" {
-  description = "SSH security group"
+  description = "SSH security group ID"
+  type        = string
+}
+
+variable "public_http_sg" {
+  description = "Public HTTP security group ID"
   type        = string
 }
 
 variable "private_http_sg" {
-  description = "Private HTTP security group"
+  description = "Private HTTP security group ID"
   type        = string
 }
