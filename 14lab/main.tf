dd9f1d19ffbfa738b0e785c33d0c07bbcbdd947c
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-08 11:18:26 +0200

    Update main.tf

--- 14lab/main.tf
+++ 14lab/main.tf
@@ -1,23 +1,20 @@
 module "network" {
   source = "./modules/network"
-  vpc_name = var.vpc_name
-  vpc_cidr = var.vpc_cidr
-  # ... передаємо всі назви сабнетів та AZ ...
+  vpc_name = "cmtr-pf5k68pq-vpc"
+  vpc_cidr = "10.10.0.0/16"
 }
 
 module "security" {
-  source = "./modules/network_security"
-  vpc_id = module.network.vpc_id
+  source           = "./modules/network_security"
+  vpc_id           = module.network.vpc_id
   allowed_ip_range = var.allowed_ip_range
-  # ... назви SG ...
 }
 
-module "app" {
-  source = "./modules/application"
+module "application" {
+  source          = "./modules/application"
   vpc_id          = module.network.vpc_id
-  public_subnets  = module.network.public_subnet_ids
+  subnet_ids      = module.network.subnet_ids
   ssh_sg_id       = module.security.ssh_sg_id
   public_http_sg  = module.security.public_http_sg_id
   private_http_sg = module.security.private_http_sg_id
-  # ... назви ASG, ALB ...
 }
