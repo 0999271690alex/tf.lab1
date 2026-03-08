dfd16b74862a23829f34ad952ffd6212b8d6dc4f
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-08 11:36:08 +0200

    Create variables.tf

---
+++ 14lab/modules/network/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_name" {
+  description = "Name tag for the VPC resource"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the network infrastructure"
+  type        = string
+}
