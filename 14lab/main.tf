aaeda2e8382c2e2ab35c008906ba6d172e4f0ee5
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-08 12:17:26 +0200

    Update main.tf

--- 14lab/main.tf
+++ 14lab/main.tf
@@ -1,20 +1,23 @@
+# 1. Модуль мережі: створює VPC, сабнети та маршрутизацію
 module "network" {
   source   = "./modules/network"
   vpc_name = "cmtr-pf5k68pq-vpc"
   vpc_cidr = "10.10.0.0/16"
 }
 
+# 2. Модуль безпеки: створює Security Groups
 module "security" {
   source           = "./modules/network_security"
   vpc_id           = module.network.vpc_id
   allowed_ip_range = var.allowed_ip_range
 }
 
+# 3. Модуль додатка: створює ALB, ASG та Launch Template
 module "application" {
-  source          = "./modules/application"
-  vpc_id          = module.network.vpc_id
-  subnet_ids      = module.network.subnet_ids
-  ssh_sg_id       = module.security.ssh_sg_id
-  public_http_sg  = module.security.public_http_sg_id
-  private_http_sg = module.security.private_http_sg_id
+  source             = "./modules/application"
+  vpc_id             = module.network.vpc_id
+  subnet_ids         = module.network.subnet_ids
+  ssh_sg_id          = module.security.ssh_sg_id
+  public_http_sg     = module.security.public_http_sg_id
+  private_http_sg_id = module.security.private_http_sg_id # Виправлено назву аргументу
 }
