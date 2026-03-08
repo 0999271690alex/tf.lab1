d61c90a0e8ebd70f12d3788909e7012c54046d43
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-08 11:36:42 +0200

    Create variables.tf

---
+++ 14lab/modules/application/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+  description = "VPC ID for target group and load balancer"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the ASG and ALB"
+  type        = list(string)
+}
+
+variable "ssh_sg_id" {
+  description = "Security Group ID for SSH access"
+  type        = string
+}
+
+variable "public_http_sg" {
+  description = "Security Group ID for the public Load Balancer"
+  type        = string
+}
+
+variable "private_http_sg" {
+  description = "Security Group ID for the private application instances"
+  type        = string
+}
