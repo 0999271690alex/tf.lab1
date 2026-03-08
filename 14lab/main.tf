acf368693e544dabf35709d48a45153e159bf034
0999271690alex <380999271690zz@gmail.com>
2026-03-08 10:29:21 +0000

    fmt

--- 14lab/main.tf
+++ 14lab/main.tf
@@ -5,17 +5,17 @@ module "network" {
 }
 
 # Назва має бути САМЕ network_security для тестів
-module "network_security" { 
+module "network_security" {
   source           = "./modules/network_security"
   vpc_id           = module.network.vpc_id
   allowed_ip_range = var.allowed_ip_range
 }
 
 module "application" {
-  source             = "./modules/application"
-  vpc_id             = module.network.vpc_id
-  subnet_ids         = module.network.subnet_ids
-  
+  source     = "./modules/application"
+  vpc_id     = module.network.vpc_id
+  subnet_ids = module.network.subnet_ids
+
   # Змінюємо module.security на module.network_security
   ssh_sg_id          = module.network_security.ssh_sg_id
   public_http_sg     = module.network_security.public_http_sg_id
