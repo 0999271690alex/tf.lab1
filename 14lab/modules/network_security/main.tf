aa777f6d97157ac12a0a43385ac65fe009dc6131
cocojambo <76431935+0999271690alex@users.noreply.github.com>
2026-03-08 11:21:05 +0200

    Create main.tf

---
+++ 14lab/modules/network_security/main.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "ssh" {
+  name   = "cmtr-pf5k68pq-ssh-sg"
+  vpc_id = var.vpc_id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "public_http" {
+  name   = "cmtr-pf5k68pq-public-http-sg"
+  vpc_id = var.vpc_id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ip_range
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_http" {
+  name   = "cmtr-pf5k68pq-private-http-sg"
+  vpc_id = var.vpc_id
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_http.id] # ВИМОГА ЛАБИ
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
